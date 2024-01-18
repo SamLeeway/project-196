c29c1e2e5e61ffe6964f645f5bece6be788d2c6a
Sam Leeway <conch.enjoyer@gmail.com>
2024-01-18 15:16:40 +0400

    First steps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::quick::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::default();
+
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "Project 196".into(),
+            ..default()
+        }),
+        ..default()
+    }));
+
+    #[cfg(debug_assertions)]
+    app.add_plugins(WorldInspectorPlugin::new());
+
+    app.run();
 }
